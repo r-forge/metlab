b8880dcac6c8df255a46acc4614d5e89fdc0be37
mariane (Mariane Machado) <mariane.machado@gmail.com>
2009-07-06 16:05:31 +0000

    Version with some object orientation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/metlab@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- metrics.r
+++ metrics.r
@@ -1,33 +1,61 @@
-# Testing SVN
 metrics <- function(path_pkg)
 {
   library(tools)
+  path_pkg = "..\\seqinr_2.0-3"
+  path_pkg = "..\\archetypes"
+  file_sep <- base:::.Platform$file.sep
 
-  #path_pkg = "..\\seqinr_2.0-3"
-  #path_pkg = "..\\archetypes_1.0"
-
-  separator <- base:::.Platform$file.sep
-  file_sep <- separator
-
-  r_files_list <- evaluate_r_files(path_pkg,file_sep)
-
-  data_files_list <- evaluate_data_files(path_pkg,file_sep)
-
+  data_list <- evaluate_data_files(path_pkg,file_sep)
+  pdf_list <- evaluate_pdf_files(path_pkg,file_sep)
+  rnw_list <- evaluate_rnw_files(path_pkg,file_sep)
+  ext_list <- evaluate_ext_files(path_pkg,file_sep)
+  number_authors <- evaluate_description(path_pkg,file_sep)
   rd_files_list <- evaluate_rd_files(path_pkg,file_sep)
+  r_files_list <- evaluate_rfiles(path_pkg,file_sep)
+  
+  rdnames <-list.files(paste(path_pkg,file_sep,"man",sep=""),pattern="[.]Rd$")
+  rnames <- list.files(paste(path_pkg,file_sep,"R",file_sep,sep=""),pattern="[.]R$")
+  
+  output <- list( "data"=data_list,
+                  "pdf"=pdf_list,
+                  "rnw"=rnw_list,
+                  "ext"=ext_list,
+                  "authors"=number_authors,
+                  "rdnames"= rdnames,
+                  "rdfiles"= rd_files_list,
+                  "rnames"= rnames,
+                  "rfiles"=r_files_list )
 
-  doc_list <- evaluate_doc_files(path_pkg,file_sep)
+  class(output) <- "metrics"
 
-  src_list <- evaluate_src_files(path_pkg,file_sep)
-
-  number_of_authors <- evaluate_description_file(path_pkg,file_sep)
-
-  tmp <- list(r_files = r_files_list,data_files = data_files_list, rd_files = rd_files_list,
-  doc_files = doc_list, src_files = src_list, number_of_authors = number_of_authors)
-
-  return(tmp)
+  return(output)
 }
 
-evaluate_description_file <- function(path_pkg,file_sep)
+print.metrics <- function(x)
+{
+  cat("Number of authors:",x$authors,"\n\n")
+  cat("Number of pdf files:",length(x$pdf),"\n",x$pdf,"\n\n")
+  cat("Number of Rnw files:",length(x$rnw),"\n",x$rnw,"\n\n")
+  cat("Number of data files:",length(x$data),"\n",x$data,"\n\n")
+  cat("Number of different extensions in the source files:",length(x$src),"\n",x$src,"\n")
+  
+  cat("\n","Analysis of each .Rd file:")
+  for(i in 1:length(x$rdnames))
+  {
+    cat("\n", x$rdnames[i],"\n")
+    print(x$rdfiles[[i]])
+  }
+  
+  cat("\n\n","Analysis of each .R file:")
+  for(i in 1:length(x$rnames))
+  {
+    cat("\n", x$rnames[i],"\n")
+    print(x$rfiles[[i]])
+  }
+  
+}
+
+evaluate_description <- function(path_pkg,file_sep)
 {
   desc_file <- readLines(paste(path_pkg,file_sep,"DESCRIPTION",sep=""), n=-1)
   for(i in 1:length(desc_file))
@@ -35,6 +63,7 @@ evaluate_description_file <- function(path_pkg,file_sep)
     if( regexpr("^[:blank:]\\{0,\\}Author:",desc_file[i],extended=F)[1] != -1 )
     {
       authors <- NULL
+      
       # Concatenating all lines that contains information about authors
       while (regexpr("^[[:blank:]]\\{0,\\}Maintainer:", desc_file[i] ,extended=F)[1] == -1)
        {
@@ -53,11 +82,10 @@ evaluate_description_file <- function(path_pkg,file_sep)
 
        return(at_counter)
     }
-    
   }
 }
 
-evaluate_src_files <- function(path_pkg,file_sep)
+evaluate_ext_files <- function(path_pkg,file_sep)
 {
   directory_info <- file.info(paste(path_pkg,file_sep,"src",sep=""))
   if (is.na(directory_info$isdir)==F)
@@ -79,190 +107,192 @@ evaluate_src_files <- function(path_pkg,file_sep)
         extension <- pieces[length(pieces)]
       }
 
-      # Check if this extension is already in the list
-      contains <- F
-      for(j in 1:length(extension_list))
-      {
-         if (extension_list[j] == extension)
-         {
-           contains <- T
-         }
-      }
-      
-      # Check if this extension is already in the blacklist
-      for(j in 1:length(blacklist))
-      {
-         if (blacklist[j] == extension)
-         {
-           contains <- T
-         }
-      }
+      # Check if this extension is already in the extension_list or blacklist
+      contains <- any( extension == c(extension_list[],blacklist[])  )
 
-      if (contains != T)
+      if (contains == F)
       {
         extension_list <- c(extension_list,extension)
       }
-
     }
-
-    tmp <- list(different_valid_src_files = length(extension_list)-1, valid_extensions = extension_list[-1])
-    return(tmp)
+    return(extension_list[-1])
   }
+  return(NULL)
 }
 
-evaluate_doc_files <- function(path_pkg,file_sep)
+evaluate_rnw_files <- function(path_pkg,file_sep)
 {
-  docs_files <- list.files(paste(path_pkg,file_sep,"inst",file_sep,"doc",file_sep,sep=""))
-  docs <- 0
-  docs_titles <- "Documents:"
-  for (i in 1:length(docs_files))
-  {
-      if( regexpr("[.]pdf$",docs_files[i],extended=F)[1] != -1 )
-      {
-        docs <- docs+1
-        docs_titles <- c(docs_titles,docs_files[i])
-      }
-  }
-  
-  tmp <- list(number_of_vignettes = docs, vignettes = docs_titles)
-  return(tmp)
+  rnw_files <- list.files(paste(path_pkg,file_sep,"inst",file_sep,"doc",file_sep,sep=""))
+  tmp <- sapply(rnw_files,regexpr,pattern="[.]Rnw$",extended=F )
+  return(names(tmp[tmp!=-1]))
 }
 
-
-evaluate_rd_files <- function(path_pkg,file_sep)
+evaluate_pdf_files <- function(path_pkg,file_sep)
 {
-  rd_files <- list.files(paste(path_pkg,file_sep,"man",sep=""))
-
-  lines <- 0
-
-  rdinfo <- NULL
-
-  for(i in 1:length(rd_files))
-  {
-    lines <- lines + length(readLines(paste(path_pkg,file_sep,"man",file_sep,rd_files[i],sep="")))
-    tmp <- analyze_rd(paste(path_pkg,file_sep,"man",file_sep,rd_files[i],sep=""))
-    rdinfo <- c(rdinfo,tmp)
-  }
-
-  general_info <- list(rd_files = length(rd_files), total_number_of_lines = lines )
-  return(general_info,rdinfo)
-
+  pdf_files <- list.files(paste(path_pkg,file_sep,"inst",file_sep,"doc",file_sep,sep=""))
+  tmp <- sapply(pdf_files,regexpr,pattern="[.]pdf$",extended=F )
+  return(names(tmp[tmp!=-1]))
 }
 
 evaluate_data_files <- function(path_pkg,file_sep)
 {
-  data_files <- list.files(paste(path_pkg,file_sep,"data",sep=""))
-
-  tmp <- list(number_data_files = length(data_files), data_files = data_files)
-  return(tmp)
+  data <- list.files(paste(path_pkg,file_sep,"data",sep=""))
+  return(unlist(data))
 }
 
-evaluate_r_files <- function(path_pkg,file_sep)
+evaluate_rd_files <- function(path_pkg,file_sep)
 {
-  r_files <- list.files(paste(path_pkg,file_sep,"R",file_sep,sep=""))
+  rdfiles <- list.files(paste(path_pkg,file_sep,"man",sep=""),pattern="[.]Rd$")
+  rdinfo <- vector("list",length(rdfiles))
 
-  lines <- 0
-  code_lines <- 0
-  just_comments <- 0
-  code_comments <- 0
-  blank <- 0
-
-  files_info <- NULL
-
-  for(i in 1:length(r_files))
+  for(i in 1:length(rdfiles))
   {
-    src_path <- paste(path_pkg,file_sep,"R",file_sep,r_files[i],sep="")
-    rinfo <- analyze_file(paste(path_pkg,file_sep,"R",file_sep,r_files[i],sep=""),r_files[i])
-    lines <- lines + rinfo$number_of_lines
-    code_lines <- code_lines + rinfo$number_of_codelines
-    just_comments <- just_comments + rinfo$number_of_intercomments
-    code_comments <- code_comments + rinfo$number_of_intracomments
-    blank <- blank + rinfo$number_of_blanks
-    files_info <- c(files_info,rinfo)
+    rdinfo[i] <- eval_each_rd(paste(path_pkg,file_sep,"man",file_sep,rdfiles[i],sep=""))
   }
-  
-  general_info <- list(number_r_files = length(r_files), total_number_of_lines = lines,
-  total_number_of_intercomments = just_comments, total_number_of_intracomments = code_comments,
-  total_number_of_codelines = code_lines, total_number_of_blank_lines = blank)
-  
-  detailed_info <- files_info
-  
-  return(list(general_info=general_info, detailed_info = detailed_info))
+
+  return(rdinfo)
 }
 
-analyze_file <- function(path_file,namefile)
+eval_each_rd <- function(path_file)
 {
-  content <- readLines(path_file, n=-1)
+  tmp <- Rd_parse(path_file)
+  
+  section <- as.vector(unlist(tmp$data[[2]]))
+  content <- as.vector(unlist(tmp$data[[1]]))
 
-  blank <- 0
-
-  for (i in 1:length(content))
+  lines <- NULL
+  characters <- NULL
+  
+  for(i in 1:length(content))
   {
-    if (content[i] == "")
+    x <- strsplit(content[i],"\n",extended=F)
+    lines <- c(lines,length(x[[1]]))
+    y <- nchar(content[i])
+    characters <- c(characters,y)
+  }
+
+  df <- data.frame(section,lines,characters)
+  
+  return(list(df))
+}
+
+evaluate_rfiles <- function(path_pkg,file_sep)
+{
+  rfiles <- list.files(paste(path_pkg,file_sep,"R",file_sep,sep=""),pattern="[.]R$")
+  rinfo <- vector("list",length(rfiles))
+
+  for(i in 1:length(rinfo))
+  {
+    path <- paste(path_pkg,file_sep,"R",file_sep,rfiles[i],sep="")
+    rinfo[[i]] <- eval_each_rfile(path)
+
+  }
+  return(rinfo)
+}
+
+eval_each_rfile <- function(path_file)
+{
+  content <- as.list(parse(path_file),n=-1)
+
+  inter_comments_vec <- NULL
+  intra_comments_vec <- NULL
+  lines_vec <- NULL
+  blank_vec <- NULL
+  characters_vec <- NULL
+  components <- NULL
+
+  # for each component
+  for(i in 1:length(content))
+  {
+    r_name <- content[i]
+    component_name <- as.character(r_name[[1]][2])
+
+    inter_comments <- 0
+    intra_comments <- 0
+    blank <- 0
+    characters <- 0
+
+    if (component_name != "NULL")
     {
-      blank <- blank+1
+      textfile <- deparse(content[[i]],control = "all", width.cutoff=500,nlines=-1)
+    
+      # for each line (of this component)
+      for (j in 1:length(textfile))
+      {
+        line_of_code <- textfile[j]
+
+        # count number of characters (excluding blank characters)
+        line_tmp <- gsub("[[:blank:]]","",line_of_code,extended=F)
+        characters <- characters + nchar(line_tmp)
+
+        # count blank lines
+        if(regexpr("^[[:blank:]]\\{0,\\}$",line_of_code,extended="F")[1] != -1)
+        {
+         blank <- blank+1
+        }
+
+        # substitute every symbol ' by a "
+        line_of_code <- gsub("\'","\"",line_of_code,extended=F)
+
+        # substitute every symbol \" by X since a \" will be always inside quotes in a correct expression
+        line_of_code <- gsub("[\\][\"]","X",line_of_code,extended=F)
+
+        # If there is a symbol # in the beggining (ignoring blank characters)
+        if( regexpr("^[[:blank:]]\\{0,\\}[#]",line_of_code,extended=F)[1] != -1 )
+        {
+          inter_comments <- inter_comments+1
+        }
+        # If not, then substitute every string (i.e "anything") by X
+        else
+        {
+          while (regexpr("\"", line_of_code ,extended=F)[1] != -1)
+          {
+            line_of_code <- sub("[\"][^\"]\\{0,\\}[\"]","X",line_of_code,extended=F)
+          }
+
+          # If still there is a # symbol, then it has a comment after some code
+          if (regexpr("#", line_of_code ,extended=F)[1] != -1)
+          {
+            intra_comments <- intra_comments+1
+          }
+        }
+      }
+
+    inter_comments_vec <- c(inter_comments_vec,inter_comments)
+    intra_comments_vec <- c(intra_comments_vec,intra_comments)
+    lines_vec <- c(lines_vec,length(textfile)-1)
+    blank_vec <- c(blank_vec,blank)
+    characters_vec <- c(characters_vec,characters)
+    components <- c(components,component_name)
     }
   }
 
-  # Lines just with comments
-  just_comments <- 0
-  
-  # Lines with comment and code
-  code_comments <- 0
 
-  for (i in 1:length(content))
+  # counting things outside components (e.g. comments)
+  rfile <- readLines(path_file, n=-1)
+  comments_outside <- 0
+  blanks_outside <- 0
+  characters_outside <- 0
+  for(i in 1:length(rfile))
   {
-    line_of_code <- content[i]
-
-    # substitute every symbol ' by a "
-    line_of_code <- gsub("\'","\"",line_of_code,extended=F)
-
-    # substitute every symbol \" by X since a \" will be always inside quotes in a correct expression
-    line_of_code <- gsub("[\\][\"]","X",line_of_code,extended=F)
-
-    # If there is a symbol # in the beggining (ignoring blank characters)
-    if( regexpr("^[[:blank:]]\\{0,\\}[#]",line_of_code,extended=F)[1] != -1 )
+    # if there is a symbol # in the beggining (ignoring blank characters)
+    if( regexpr("^[[:blank:]]\\{0,\\}[#]",rfile[i],extended=F)[1] != -1 )
     {
-      just_comments <- just_comments+1
-    }
-    # If not, then substitute every string (i.e "anything") by X
-    else
-    {
-       while (regexpr("\"", line_of_code ,extended=F)[1] != -1)
-       {
-         line_of_code <- sub("[\"][^\"]\\{0,\\}[\"]","X",line_of_code,extended=F)
-       }
-
-       # If still there is a # symbol, then it has a comment after some code
-       if (regexpr("#", line_of_code ,extended=F)[1] != -1)
-       {
-          code_comments <- code_comments+1
-       }
-
+      comments_outside <- comments_outside + 1
+      blanks_outside <- blanks_outside + 1
+      characters_outside <- characters_outside + nchar(rfile[i])
     }
   }
-  # count the number of lines with code
-  code_lines <- length(content) - (blank+just_comments)
 
-  return(list(name = namefile, number_of_lines = length(content), number_of_codelines = code_lines, number_of_intercomments = just_comments,
-  number_of_intracomments = code_comments, number_of_blanks = blank))
+
+  components <- c(components,"Total(including lines outside the functions):")
+  lines_vec <- c(lines_vec,length(rfile))
+  blank_vec <- c(blank_vec,blanks_outside+sum(blank_vec))
+  inter_comments_vec <- c(inter_comments_vec,comments_outside)
+  intra_comments_vec <- c(intra_comments_vec,sum(intra_comments_vec))
+  characters_vec <- c(characters_vec,characters_outside+sum(characters_vec))
+
+  return(data.frame("component"=components,"lines"=lines_vec,"blank lines"=blank_vec,
+                    "inter"=inter_comments_vec,"intra"=intra_comments_vec,"characters"=characters_vec))
+
 }
-
-
-analyze_rd <- function(path_file)
-{
-  tmp <- Rd_parse(path_file)$data
-  info <- NULL
-  for(i in 1:length(tmp[[1]]))
-  {
-    #maybe count number of characters instead of lines ...
-    x <- strsplit(tmp[[1]][i],"\n",extended=F)
-    size <- length(x[[1]])
-    info <- c(info,size)
-  }
-  
-  r <- as.list(info)
-  
-  return(list(info,tmp[[2]]))
-}
-
